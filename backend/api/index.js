f85c0702584c2e9026fc147e2887cfec04994da8
Bass913 <bastiendikiadi@outlook.fr>
2024-06-06 12:53:30 +0200

    Database linked

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,18 +1,27 @@
 require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
+const mongoose = require('mongoose');
 const port = process.env.PORT;
 
 const app = express();
 
+const connection = async () => {
+    try {
+        await mongoose.connect(process.env.DB_NOSQL_HOST);
+        console.log('database connected');
+    } catch (err) {
+        console.log(err);
+    }
+};
+
 app.listen(port || 3000, () => {
     console.log(`Clean Code app listening on port ${port}`);
+    connection();
 });
 
 app.use(cors());
 
 app.use(express.json()); // application/json
 
-app.listen(port || 3000, () => {
-    console.log(`Challenge S2 app listening on port ${port}`);
-});
+
